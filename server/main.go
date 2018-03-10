ae60ab70b7b4f6fe25f6fb2edbe7e88f9fa24380
Zihan Yang <whois.zihan.yang@gmail.com>
2018-03-10 15:48:24 +0800

    get mutex durint server startup

--- server/main.go
+++ server/main.go
@@ -9,6 +9,12 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
+	"time"
+	"math/rand"
+
+	. "redsync"
+
+	"github.com/garyburd/redigo/redis"
 )
 
 type arrayFlags []string
@@ -168,11 +174,44 @@ func mycarts(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	w.Write(jData)
 }
 
+var addrs = [5]string{
+        "127.0.0.1:6666",
+        "127.0.0.1:6667",
+        "127.0.0.1:6668",
+        "127.0.0.1:6669",
+        "127.0.0.1:6670",
+}
+
+func newMockPools() []Pool {
+        pools := []Pool{}
+        for i := 0; i < 5; i++ {
+                pools = append(pools, &redis.Pool{
+                                MaxIdle:     3,
+                                IdleTimeout: 240 * time.Second,
+                                Dial: func() (redis.Conn, error) {
+                                        return redis.Dial("udp", addrs[i]) //TODO. maybe TCP or unix is better?
+                                },
+                                TestOnBorrow: func(c redis.Conn, t time.Time) error {
+                                        _, err := c.Do("PING")
+                                        return err
+                                },
+                        })
+        }
+        return pools
+}
+
 func main() {
 	flag.Usage = usage
 	flag.Parse()
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
+	pools := newMockPools();
+	rs := New(pools)
+	mutex := rs.NewMutex(fmt.Sprintf("test-redsync%d", rand.Intn(100000)))
+	if mutex != nil {
+		//just for test
+		fmt.Println("get mutex done!")
+	}
 	consistency.Start(getIPAddress(), comuport, nodes)
 
 	router := httprouter.New()
