a47635c2e6e89526329ba01b002359567fd4c87d
Zihan Yang <whois.zihan.yang@gmail.com>
2018-03-10 22:41:50 +0800

    Bug fix and parameter tuning
    1. Fix bug in consistency/mutex that would cause crash when doing mutex.Lock()
    2. Change the paramter in redsync to improve performance. It's ok for only 3 sites
    3. reduce 5 redis servers to 3 to improve performance

--- consistency/mutex.go
+++ consistency/mutex.go
@@ -3,30 +3,29 @@ package consistency
 import (
 	"fmt"
 	"time"
-	"math/rand"
 	. "redsync"
 
         "github.com/garyburd/redigo/redis"
 )
 
-var addrs = [5]string{
+var addrs = []string{
         "127.0.0.1:6666",
         "127.0.0.1:6667",
         "127.0.0.1:6668",
-        "127.0.0.1:6669",
-        "127.0.0.1:6670",
+        //"127.0.0.1:6669",
+        //"127.0.0.1:6670",
 }
 var mutex *Mutex
 var pools []Pool
 
 func newMockPools() []Pool {
         pools = []Pool{}
-        for i := 0; i < 5; i++ {
+        for _, addr := range addrs {
                 pools = append(pools, &redis.Pool{
                                 MaxIdle:     3,
                                 IdleTimeout: 240 * time.Second,
                                 Dial: func() (redis.Conn, error) {
-                                        return redis.Dial("udp", addrs[i]) //TODO. maybe TCP or unix is better?
+                                        return redis.Dial("unix", addr) //TODO. maybe TCP or unix is better?
                                 },
                                 TestOnBorrow: func(c redis.Conn, t time.Time) error {
                                         _, err := c.Do("PING")
@@ -38,9 +37,9 @@ func newMockPools() []Pool {
 }
 
 func MutexInit() {
-	pools := newMockPools();
+	pools = newMockPools();
         rs := New(pools)
-        mutex = rs.NewMutex(fmt.Sprintf("test-redsync%d", rand.Intn(100000)))
+        mutex = rs.NewMutex("test-redsync")
         if mutex != nil {
                 //just for test
                 fmt.Println("get mutex done!")
--- redcluster/main.go
+++ redcluster/main.go
@@ -7,8 +7,9 @@ import (
 )
 
 func main() {
-	for i := 0; i < 5; i++ {
-                server, err := tempredis.Start(tempredis.Config{"databases": "5", "port" : fmt.Sprintf("%d", 6666 + i)})
+	for i := 0; i < 3; i++ {
+                //server, err := tempredis.Start(tempredis.Config{"databases": "5", "port" : fmt.Sprintf("%d", 6666 + i)})
+                server, err := tempredis.Start(tempredis.Config{"port" : fmt.Sprintf("%d", 6666 + i)})
                 if err != nil {
                         panic(err)
                 }
